6e038ee62acd6841ebd7c58653310433a8f3d910
basheer mulla <basheer1067@gmail.com>
2021-02-26 23:55:43 +0200

    Replacing the dashboard with RTL template

--- dashboard/src/app/app-routing.module.ts
+++ dashboard/src/app/app-routing.module.ts
@@ -1,10 +1,43 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule }                     from '@angular/core';
+import { RouterModule, Routes }         from '@angular/router';
 
-const routes: Routes = [];
+import { DefaultLayoutComponent }       from './layouts/default/default.component';
+
+import { PageDashboardComponent }       from './pages/dashboard/dashboard.component';
+
+const defaultRoutes: Routes = [
+  { path: 'dashboard', component: PageDashboardComponent },
+];
+
+const extraRoutes: Routes = [
+
+];
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/default-layout/dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'default-layout',
+    component: DefaultLayoutComponent,
+    children: defaultRoutes
+  },
+  /*{
+    path: 'extra-layout',
+    component: ExtraLayoutComponent,
+    children: extraRoutes
+  },*/
+  {
+    path: '**',
+    component: DefaultLayoutComponent,
+    children: defaultRoutes
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
